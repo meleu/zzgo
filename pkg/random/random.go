7cadfd8bd4f2a14cdd3d236036539c4f24939d4f
meleu <8508804+meleu@users.noreply.github.com>
2026-04-15 10:44:30 -0300

    Add doc comments to random package

--- pkg/random/random.go
+++ pkg/random/random.go
@@ -1,3 +1,20 @@
+// Package random provides a random number generator.
+//
+// Create a generator (seeded with the current time):
+//
+//	g := random.New()
+//	n := g.Int(1, 10) // random int in [1, 10]
+//
+// Create a generator with a fixed seed for reproducible output:
+//
+//	g := random.NewWithCustomSeed(42)
+//	n := g.Int(-5, 5) // random int in [-5, 5]
+//
+// The bounds passed to Int may be given in any order; Int(10, 1) is
+// equivalent to Int(1, 10).
+//
+// Inspired by:
+// https://github.com/funcoeszz/funcoeszz/blob/master/zz/zzaleatorio.sh
 package random
 
 import (
@@ -21,6 +38,8 @@ func NewWithCustomSeed(seed int64) *Generator {
 	}
 }
 
+// Int returns a pseudo-random integer in the inclusive range bounded by
+// number1 and number2. The bounds may be provided in either order.
 func (g *Generator) Int(number1, number2 int) int {
 	minVal := number1
 	maxVal := number2
